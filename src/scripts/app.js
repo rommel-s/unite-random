abee54e86e9dc6598ff5304d40bfe4c84e9d878a
Rommel Santos <rommelrenne@gmail.com>
2022-05-30 21:17:51 -0300

    Settings for PWa

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -29,3 +29,39 @@ function startShufflingAnimation() {
   poke.innerHTML = pokes[randomLetter].name;
   pokeImage.src = pokes[randomLetter].source;
 }
+
+//SERVICE WORKER
+
+let newWorker;
+
+function showUpdateBar() {
+  let snackbar = document.getElementById('snackbar');
+  snackbar.className = 'show';
+}
+
+// The click event on the pop up notification
+document.getElementById('reload').addEventListener('click', function(){
+  newWorker.postMessage({ action: 'skipWaiting' });
+});
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./sw.js").then(registration => {
+    registration.addEventListener("updatefound", () => {
+      newWorker = registration.installing;
+      newWorker.addEventListener("statechange", () => {
+        switch (newWorker.state) {
+          case "installed":
+            if (navigator.serviceWorker.controller) {
+              showUpdateBar()
+            }
+            break;
+        }
+      })
+    })
+    console.log("Service worker registered!")
+    console.log(registration)
+  }).catch(error => {
+    console.log("Registration failed!")
+    console.log(error)
+  })
+}
